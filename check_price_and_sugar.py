6f58069df2b153943a91ead7288f1509348adb57
bomond <deshev@gmail.com>
2023-02-25 15:17:06 +0100

    edit fig. 1

--- check_price_and_sugar.py
+++ check_price_and_sugar.py
@@ -2,7 +2,12 @@
 # -*- coding: utf-8 -*-
 """
 Created on Sat Feb 25 12:53:08 2023
-@author: tazio
+@author: BD
+
+- Check if the sugar content and the price define how often a given candy is
+chosen when compared with one other candy from the list (85 in total)
+- Plot the results
+
 """
 import pandas as pd
 import numpy as np
@@ -34,6 +39,7 @@ def check_price_and_sugar(tbl):
     fit = np.polyfit(x=tbl['sugarpercent'], y=tbl['winpercent'], deg=1)
     p = np.poly1d(fit)
 
+    # Initiate the figure
     plt.figure(figsize=(10,5))
     ax1 = plt.subplot(1,2,1)
     
@@ -43,15 +49,20 @@ def check_price_and_sugar(tbl):
     ax1.plot([sugar_min, sugar_max], p([sugar_min, sugar_max]), \
         color='gray', lw=2, zorder=1)
 
+    # Map the winpercentage to a colormap (needed because of the custom symbols)
     cmap = cm.plasma
-    norm = matplotlib.colors.Normalize(vmin=tbl['winpercent'].min(), vmax=tbl['winpercent'].max(), clip=True)
+    norm = matplotlib.colors.Normalize(vmin=tbl['winpercent'].min(),\
+                                       vmax=tbl['winpercent'].max(), clip=True)
     
     # Plot the data points
     for i in range(len(tbl)):
-        draw_a_candy(tbl['sugarpercent'][i], tbl['winpercent'][i], cmap(norm(tbl['winpercent'][i])), s=tbl['winpercent'][i])
+        draw_a_candy(tbl['sugarpercent'][i], tbl['winpercent'][i],\
+                     cmap(norm(tbl['winpercent'][i])), s=tbl['winpercent'][i])
     
+    # Format the plot
     ax1.set_xlabel('sugar content [% of the highest]')
     ax1.set_ylabel('win [%]')
+    # The title shows the correlation coefficient
     ax1.set_title('correlation = {:.2f}'.format(corr_coeficients['winpercent']['sugarpercent']))
     ax1.tick_params(axis='both', direction='in', which='both', right='on', top='on')
 
@@ -62,6 +73,7 @@ def check_price_and_sugar(tbl):
     p = np.poly1d(fit)
 
     ax2 = plt.subplot(1,2,2)
+    
     # Plot the correlation line
     sugar_min = tbl['pricepercent'].min()
     sugar_max = tbl['pricepercent'].max()
@@ -70,14 +82,18 @@ def check_price_and_sugar(tbl):
 
     # Plot the data points
     for i in range(len(tbl)):
-        draw_a_candy(tbl['sugarpercent'][i], tbl['winpercent'][i], cmap(norm(tbl['winpercent'][i])), s=tbl['winpercent'][i])
+        draw_a_candy(tbl['sugarpercent'][i], tbl['winpercent'][i],\
+                     cmap(norm(tbl['winpercent'][i])), s=tbl['winpercent'][i])
     
+    # Format the plot
     ax2.set_yticklabels([])
     ax2.set_xlabel('price [% of the highest]')
     ax2.set_ylabel("")
+    # The title shows the correlation coefficient
     ax2.set_title('correlation = {:.2f}'.format(corr_coeficients['winpercent']['pricepercent']))
     ax2.tick_params(axis='both', direction='in', which='both', right='on', top='on')
 
+    # Save it
     plt.tight_layout()
     plt.savefig(path+'check_sugar_price.png', dpi=300)
 
